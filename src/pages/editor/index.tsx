77dcf7ab21280d0f367532c004b3b7b56bfd8a6e
马琪斌 <maqi1520@163.com>
2022-03-25 13:23:17 +0800

    dynamic add loader

--- src/pages/editor/index.tsx
+++ src/pages/editor/index.tsx
@@ -2,6 +2,7 @@ import dynamic from 'next/dynamic';
 
 const Editor = dynamic(() => import('../../components/editor'), {
   ssr: false,
+  loading: () => <div className="loader"></div>,
 });
 
 export default function EditorPage() {
